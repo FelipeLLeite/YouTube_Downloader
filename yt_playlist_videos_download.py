e3c341eb3b5ce995881b5540e388a75552ce8205
FELIPE LOPES LEITE <61467773+FelipeLLeite@users.noreply.github.com>
2021-05-25 20:17:00 -0400

    Update yt_playlist_videos_download.py
    Add comments and guides

--- yt_playlist_videos_download.py
+++ yt_playlist_videos_download.py
@@ -2,15 +2,25 @@ from pytube import Playlist
 
 
 def main():
-    url: str = "https://www.youtube.com/playlist?list=PLTD74k-jahHwX7ogz74-XYdxERX4AWj_8"
+    url: str = # Insert the url of the playlist you want to download
     try:
+        # Create a object of playlist
         playlist = Playlist(url)
+        # Loop through all the videos from the playlist
         for video in playlist.videos:
+            # After create a stream from each child of the parent object
+            # The streams parameter, according to pytube official documentation,
+            # "Interface to query both adaptive (DASH) and progressive streams."
+            # Now we can call the first() method. This method return:
+            # "the first result of this query or None if the result doesn’t contain any streams."
+            # To finalize is to download the video that exist in the streams parameter in the 
+            # video obj (Youtube instance)
             video.streams.first().download()
     except Exception as e:
+        # If any Exception occurs
         print(e)
         raise Exception('Some exception occurred.')
-    print('All YouTube audio downloaded successfully.')
+    print('Everything downloaded successfully.')
 
 
 if __name__ == "__main__":
